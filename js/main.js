d52c629bcd6f4dea76f3f88ec45079d9fa097914
xomyak-coder <dkhol@inbox.ru>
2021-11-24 20:22:01 +0300

    create: added reviews section and fixed slider

--- js/main.js
+++ js/main.js
@@ -16,3 +16,21 @@ const hotelSwiper = new Swiper('.hotel-slider', {
   }
 
 });
+const reviewsSwiper = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+
+  effect: "coverFlow",
+
+  keyboard: {
+    enabled: true,
+    pageUpDown: true
+  }
+
+});
